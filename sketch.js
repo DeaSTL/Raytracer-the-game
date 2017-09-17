6d091d6072aec34d2e79bff53b6d2a3758815b66
DeaSTL <jhartway99@gmail.com>
2017-09-17 10:07:16 -0400

    Final Release

--- sketch.js
+++ sketch.js
@@ -7,7 +7,9 @@ var bonus_round_enemy_spawn_rate = 5
 var score_incrementor = 1;                            
 var score_required_bonus_round = 100;
 var bonus_round = false;
-var score_point_spawn_rate = 200;
+var score_point_spawn_rate = 150;
+var imagine_font;
+var game_running = true;
 ////////////////////////
 
 //Global entities
@@ -19,7 +21,19 @@ var enemy_list = [];
 var float_text_list = [];
 var entity_list = []; 
 /////////////////////////
-
+function ResetGame(){
+	score_incrementor = 1;
+	score_required_bonus_round = 100;
+	bonus_round = false;
+	score_point_spawn_rate = 150;
+	game_running = true;
+	main_player.score = 100;
+	main_player.x = windowWidth/2
+	enemy_spawn_rate = 14;
+	bonus_round_enemy_spawn_rate = 5;
+	entity_list = [];
+	textSize(32);
+}
 class RectangleCollisionObject{
 	constructor(x,y){
 		this.x = x;
@@ -47,6 +61,9 @@ class Entity extends RectangleCollisionObject {
 		this.to_delete = false;
 		this.is_collidable = true;
 	}
+	update(){
+
+	}
 	draw(){
 
 	}
@@ -62,11 +79,22 @@ class Player extends RectangleCollisionObject {
 		super(x,y);
 		this.width = 100;
 		this.height = 100;
-		this.score = 0;
+		this.score = 100;
 		this.speed = 0;
-		this.maxSpeed = 5;	
+		this.max_speed = 10;
+		this.direction = false;
+		this.is_moving = false;	
 	}
+	
 	draw(){
+		if(this.direction){
+			this.speed = this.max_speed;
+		}else{
+			this.speed = -this.max_speed;
+		}
+		if(!this.is_moving){
+			this.speed = 0;
+		}
 		if(this.x < 0){
 			this.speed = 0;
 			this.x = 0;
@@ -101,6 +129,7 @@ class FloatText extends Entity {
 
 	}
 	draw(){
+		
 		this.life_span -= 1;
 		this.brightness -= this.life_span_decrememtor;
 		fill(255);
@@ -116,32 +145,50 @@ class Enemy extends Entity {
 		super(x,y);
 		this.width = 25;
 		this.height = 100;
-		this.speed = (Math.random()*20)+5;
+		this.speed = (Math.random()*40)+5;
 		this.tail_length = this.speed;
-		this.damage = Math.round(this.speed/2);
+		this.damage = Math.round(this.speed);
+		this.color = color(360,0,0);
+		this.saturation = 0;
+		this.hue = 360;
+
+
 	}
+
 	draw(){
-		for(var i = 0;i<Math.round(this.tail_length);i++){
-			if(bonus_round){
-				
-				fill(color(this.rand_color_value,255,i*15))
-			}else{
-				
-				fill(color(255,0,i*15))
-			}
-			rect(this.x,this.y+(i*20)-200,this.width,this.height);
+		
+		for(var i = 0;i<Math.round(this.tail_length);i++){	
+			fill(color(this.hue,this.saturation,lightness(this.color)+(i*3)));
+			
+			rect(this.x,this.y+(i*20)-(this.tail_length*10),this.width,this.height);
 		}
 		this.y += this.speed;
 		if(this.collides(main_player)){
 			this.to_delete = true;
 			addNewFloatText("-"+this.damage,this.x,this.y,100);
 			main_player.score-=this.damage;
-		}		
+		}
+		this.update();		
 	}
 	out_of_bounds(){
 		return  this.y > windowHeight+300;
 	}
 }
+class RainbowEnemy extends Enemy{
+	constructor(x,y){
+		super(x,y);
+		this.saturation = 100;
+		this.hue = Math.random()*360;
+		this.damage *= 2;
+	}
+	update(){
+		this.hue += 4;
+		if(this.hue > 360){
+			this.hue = 0;
+		}
+	}
+}
+
 class ScorePoint extends Entity{
 	constructor(x,y){
 		super(x,y)
@@ -149,7 +196,7 @@ class ScorePoint extends Entity{
 		this.width = 25;
 		this.height = 25;
 		this.speed = 10;
-		this.color = color('hsb(59, 100%, 50%)');
+		this.color = color(59,100,50);
 	}
 	draw(){
 		this.y += this.speed;
@@ -174,6 +221,10 @@ function GenerateEntities(){
 		rand_value = Math.round(Math.random()*row_count);
 		entity_list.push(new Enemy(rand_value*seperation_width,-200))		
 	}
+	if(frameCount%(enemy_spawn_rate*5) == 0){
+		rand_value = Math.round(Math.random()*row_count);
+		entity_list.push(new RainbowEnemy(rand_value*seperation_width,-200))		
+	}
 	if(frameCount%score_point_spawn_rate == 0){
 		rand_value = (Math.random()*row_count);
 		entity_list.push(new ScorePoint(rand_value*seperation_width,-200));
@@ -190,92 +241,98 @@ function DrawEntities(){
 		if(current_entity.out_of_bounds()){
 			entity_list.splice(i,1);
 		}
-		// if(main_player.score == score_required_bonus_round){
-		// 	bonus_round = true;
-		// 	score_incrementor = 5;
-		// }
-		// if(main_player.score == score_required_bonus_round+100){
-		// 	bonus_round = false;
-		// 	score_required_bonus_round *= 2;
-		// 	score_incrementor = 1;
-		// }
-		
-		// if(current_entity.y >= windowHeight+200){
-		// 	if(bonus_round){
-		// 		addNewFloatText("+5",current_entity.x,windowHeight+10);
-		// 		main_player.score += 5;
-		// 	}else{
-		// 		addNewFloatText("+1",current_entity.x,windowHeight+10);
-		// 		main_player.score += 1;
-		// 	}			
-		// 	enemy_list.splice(i,1);
-
-		// }
-		// if(bonus_round){
-		// 	enemy_spawn_rate = 10;
-		// }else{
-		// 	enemy_spawn_rate = 20;
-		// }
-		
+	}
+	if(main_player.score < 1){
+		game_running = false;
 	}
 }
 
 function addNewFloatText(input_text,x,y){
 	entity_list.push(new FloatText(input_text,x,y,100))
 }
-
+function preload(){
+	imagine_font = loadFont('./imagine_font.otf');
+}
 
 function setup(){
 	main_player = new Player(windowWidth/2,windowHeight-100)
 	createCanvas(windowWidth-10,windowHeight-10);
 	//noFill();
+	textFont(imagine_font);
 	textSize(32);
 	noStroke();
-	colorMode(HSB,255);
+	colorMode(HSL,360,100,100);
 	frameRate(60);
 		
 }
 
 
 function draw(){
-	GenerateEntities();
-			
-	background(0);
-	DrawEntities();
-	main_player.draw();
-	fill(255);
 	
-	if(main_player.score < 1){
-		fill(color('hsb(0,99.51%,70.57%)'));
-		text("Score: "+main_player.score,100,100+35);
-	}else if(main_player.score > 0){
-		fill(color('hsl(84, 100%, 40%)'));
-		text("Score: "+main_player.score,100,100+35);	
+	if(game_running){
+
+		GenerateEntities();
+		background(0);
+		DrawEntities();
+		main_player.draw();
+		fill(255);
+		
+		if(main_player.score < 1){
+			fill(color('hsb(0,99.51%,70.57%)'));
+			text("Score: "+main_player.score,100,100+35);
+		}else if(main_player.score > 0){
+			fill(color('hsl(84, 100%, 40%)'));
+			text("Score: "+main_player.score,100,100+35);	
+		}
+		fill(255);
+	}else{
+		game_over = "Game Over";
+
+		textSize(windowWidth/game_over.length);
+		background(0);
+		x = (textWidth(game_over))-(windowWidth/2);
+		y = (windowHeight/2)-textSize();
+		width = textWidth(game_over);
+		height = textSize();
+
+		if(collidePointRect(mouseX,mouseY,x,y,width,height)){
+			fill(98,100,42);
+			text("Game Over",(textWidth(game_over))-(windowWidth/2),windowHeight/2);
+			if(mouseIsPressed){
+				ResetGame();	
+			}
+		}else{
+			fill(0,0,100);
+			text("Game Over",(textWidth(game_over))-(windowWidth/2),windowHeight/2);
+
+		}
+
 	}
-	fill(255);
-	
-	text("Points till next bonus: "+(score_required_bonus_round-main_player.score),100,100+35+35);
+	//text("Points till next bonus: "+(score_required_bonus_round-main_player.score),100,100+35+35);
 
 			
 }
 
 function keyPressed(){
 	if(keyCode == RIGHT_ARROW){
-		main_player.speed = main_player.maxSpeed;
+		main_player.direction = true;
+		main_player.is_moving = true;
 	}
 	if(keyCode == LEFT_ARROW){
-		main_player.speed = -main_player.maxSpeed;
+		main_player.direction = false;
+		main_player.is_moving = true;
 	}	
 }
 
 function keyReleased(){
-	if(keyCode == RIGHT_ARROW){
-		main_player.speed = 0;
+	if(keyCode == RIGHT_ARROW || keyCode == LEFT_ARROW){
+		main_player.is_moving = false;
 	}
-	if(keyCode == LEFT_ARROW){
-		main_player.speed = 0;
-	}
+	
 }
 function windowResized(){
-	resizeCanvas(windowWidth,windowHeight)
+	resizeCanvas(windowWidth-10,windowHeight-10);
+	main_player.x = windowWidth/2;
+	main_player.y = windowHeight-100;
+
 }
